33d02aaa6daacb78cc730d8984920c2a024ceb1c
Zhaolin Lau <zhaolinlau@gmail.com>
2024-05-14 02:11:32 +0800

    implement verified middleware to home route

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Auth::routes();
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['verified']);
