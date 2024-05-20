25126ece41f99fae91afbcff92d49e5328b9cec1
Zhaolin Lau <zhaolinlau@gmail.com>
2024-05-21 00:38:58 +0800

    add homepage image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center py-4">
-        <div class="col-md-8">
-            <div class="fs-1 fw-bold text-center">
-							Welcome {{ auth()->user()->name }}!
-						</div>
-        </div>
-    </div>
-</div>
+	<div class="container">
+		<div class="row justify-content-center py-4">
+			<div class="col-md-8">
+				<div class="fs-1 fw-bold text-center">
+					Welcome {{ auth()->user()->name }}!
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6 mx-auto">
+				<img class="img-fluid" src="img/welcome.svg" alt="welcome.svg">
+			</div>
+		</div>
+	</div>
 @endsection
